60de992ce821875989d8c65a2ba13d049e6a8036
M4RKL4B0Y <markvlaboy@gmail.com>
2024-01-21 23:33:00 -0500

    add README

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
-app.use('places', require('./controllers/places'))
+app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.send('Hell world!')
+    res.send('Hello world!')
 })
 
 app.get('*', (req, res) => {
