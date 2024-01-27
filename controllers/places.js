59314f504ae716d83a48d8dc9ac20d6605064adf
M4RKL4B0Y <markvlaboy@gmail.com>
2024-01-26 22:29:53 -0500

    adding Home and 404 page  views

--- controllers/places.js
+++ controllers/places.js
@@ -1,8 +1,8 @@
 const router = require('express').Router()
 
 router.get('/', (req, res) => {
-    // res.render('places/index')
-    res.send('places/index')
+    res.render('places/index')
+    // res.send('places/index')
 })
 
 module.exports = router
--- index.js
+++ index.js
@@ -1,18 +1,19 @@
+// Modules and Globals
 require('dotenv').config()
-
 const express = require('express')
+// const methodOverride = require('method-overide')
 const app = express()
 
-
+//Express Settings
 app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine)
+app.engine('jsx', require('express-react-views').createEngine())
 
 app.use('/places', require('./controllers/places'))
 
 //HOME
 app.get('/', (req, res) => {
-    res.send('Home')
-    // res.render('Home')
+    // res.send('home')
+    res.render('home')
 })
 
   
@@ -20,8 +21,8 @@ app.get('/', (req, res) => {
 //404
 app.get('*', (req, res) => {
     // res.status(404).send('<h1>404 Page</h1>')
-    // res.render('error404')
-    res.send('error404')
+    res.render('error404')
+    // res.send('error404')
 })
 
 app.listen (process.env.PORT)
