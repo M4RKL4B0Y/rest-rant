58a1cfc70909b440ac33ee94e174536acadb087d
M4RKL4B0Y <markvlaboy@gmail.com>
2024-02-12 22:33:43 -0500

    new places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,18 +1,42 @@
 const router = require('express').Router()
-const places = require('../models/places')
+const places = require('../models/places.js')
 
 router.get('/new', (req, res) => {
     res.render('places/new')
   })
   
   router.post('/', (req, res) => {
-    console.log(req.body)
-    res.send('POST /places')
+    // console.log(req.body)
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
   })
+  
   
 
 router.get('/', (req, res) => {
-    let places = []
+    let places = [{
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: '/images/table-in-restaurant.png'
+      }, {
+          name: 'Coding Cat Cafe',
+          city: 'Phoenix',
+          state: 'AZ',
+          cuisines: 'Coffee, Bakery',
+          pic: '/images/coffee-cat.jpg'
+      }]
     res.render('places/index', { places })
     // res.send('places/index')
 })
--- models/places.js
+++ models/places.js
@@ -1,18 +1,13 @@
-// GET /places
-// app.get('/', (req, res) => {
-  let places = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: '/images/table-in-restaurant.png'
-  }, {
-      name: 'Coding Cat Cafe',
-      city: 'Phoenix',
-      state: 'AZ',
-      cuisines: 'Coffee, Bakery',
-      pic: '/images/coffee-cat.jpg'
-  }]
-  
-//       res.render('places/index', { places })
-//   })  
+module.exports = [{
+  name: 'H-Thai-ML',
+  city: 'Seattle',
+  state: 'WA',
+  cuisines: 'Thai, Pan-Asian',
+  pic: '/images/table-in-restaurant.png'
+}, {
+  name: 'Coding Cat Cafe',
+  city: 'Phoenix',
+  state: 'AZ',
+  cuisines: 'Coffee, Bakery',
+  pic: '/images/coffee-cat.jpg'
+}]
