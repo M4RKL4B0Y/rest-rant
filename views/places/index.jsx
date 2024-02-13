902a647b00726674ff305ee16499e95f8342bb95
M4RKL4B0Y <markvlaboy@gmail.com>
2024-02-12 22:49:49 -0500

    show route connected

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -5,10 +5,14 @@ const Def = require('../default')
 
 
 function index (data) {
-  let placesFormatted = data.places.map((place) => {
+  let placesFormatted = data.places.map((place, index) => {
     return (
       <div className="col-sm-6">
-        <h2>{place.name}</h2>
+        <h2>
+          <a href={`/places/${index}`} >
+            {place.name}
+          </a>
+        </h2>
         <p className="text-center">
           {place.cuisines}
         </p>
@@ -19,6 +23,7 @@ function index (data) {
       </div>
     )
   })
+  
   
   return (
     <Def>
