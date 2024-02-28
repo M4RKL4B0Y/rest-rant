de1ad83c8b4cc8bebf66def179786795d94053d1
M4RKL4B0Y <markvlaboy@gmail.com>
2024-02-27 21:16:58 -0500

    complete

--- index.js
+++ index.js
@@ -1,34 +1,29 @@
 // Modules and Globals
 require('dotenv').config()
 const express = require('express')
-const app = express()
 const methodOverride = require('method-override')
+const app = express();
 
-//Express Settings
-// app.set('views', _dirname + '/views')
+// Express Settings
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
-//Controllers & Routes
+// Controllers & Routes
+
+app.use(express.urlencoded({ extended: true }))
+
 app.use('/places', require('./controllers/places'))
 
-//HOME
 app.get('/', (req, res) => {
-    // res.send('home')
     res.render('home')
 })
 
-
-
-//404
 app.get('*', (req, res) => {
-    // res.status(404).send('<h1>404 Page</h1>')
     res.render('error404')
-    // res.send('error404')
 })
 
-app.listen (process.env.PORT)
-
+// Listen for Connections
+app.listen(process.env.PORT)
